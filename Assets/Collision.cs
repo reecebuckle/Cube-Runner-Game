12d444b7b733c3c181543ad0fcc52144763310d3
Reece Buckle <rtb1c17@soton.ac.uk>
2020-09-21 14:25:27 +0100

    Adds collision behaviour between player and obstacles

---
+++ Assets/Collision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Collision : MonoBehaviour {
+
+    public PlayerMovement movement;
+    // Start is called before the first frame update
+    void onCollisionEnter(Collision info) {
+        //prints to log what we have collided with
+        if (info.GetComponent<Collider>().tag == "Obstacle") {
+            Debug.Log("We hit an obstacle!");
+            //stop moving if hit an object
+            movement.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
