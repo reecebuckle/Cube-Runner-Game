3f76cc5044479cfcbb5cd7c4786a08fe902c00d8
Reece Buckle <rtb1c17@soton.ac.uk>
2020-09-21 12:30:47 +0100

    Initial Commit

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+   public Transform player;
+   public Vector3 offset;//stores 3 floats (x,y,z)
+
+    // Update is called once per frame
+    void Update() {
+        //+ adding two vectors here!
+        transform.position = player.position + offset;
+        
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+public Rigidbody rb;
+
+//variables to handle forwards and sideways force. The 'f' is to identify these are floats
+public float forwardForce = 2000f;
+public float sidewaysForce = 500f;
+//public float jumpHeight = 3f;
+
+//public float distToGround = 0.5f;
+
+    //oid Start() {
+    //    rb = GetComponent<Rigidbody> ();
+    
+    //}
+
+    // Unity prefers fixed update when using physics collisions 
+    void FixedUpdate() {
+        //add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        //If user inputs D on keyboard
+        if (Input.GetKey("d")) {
+            //apply force on object in postive x direction (to right)
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+         if (Input.GetKey("a")) {
+            //move left
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+       // if (Input.GetKey(KeyCode.Space) && isGrounded()) {
+            //Vector3 jumpVelocity = new Vector3 (0f, jumpSpeed, 0f);
+            //rb.velocity = rb.velocity + jumpVelocity; 
+            //velocity.y = Mathf.Sqrt(jumpHeight * - 2f * gravity);
+
+       // }
+
+        //float horizontalVelocity = Input.GetAxis("Horizontal");
+       // float verticalVelocity = Input.GetAxis("Vertical");
+        //Vector3 movement = new Vector3 (horizontalVelocity * 10f * Time.deltaTime, 0, verticalVelocity * 10f * Time.deltaTime);
+       
+    }
+     //bool isGrounded () {
+      //      return Physics.Raycast (transform.position, Vector3.down, distToGround);
+      //  }
+       
+}
